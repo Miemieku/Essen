958b9db001ffbf852d2fe7146a5c3fb158388c3d
JW_He <61524560+Miemieku@users.noreply.github.com>
2025-01-30 12:08:24 +0100

    Update airQuality.js

--- public/airQuality.js
+++ public/airQuality.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://datenplattform-essen.netlify.app/.netlify/functions";
+const API_BASE_URL = "https://datenplattform-essen.netlify.app/.netlify/functions/ubaProxy";
 const stations = ["DENW134", "DENW043", "DENW247", "DENW024"];
 let mapMarkers = {}; // 存储测量站点的 Marker
 
